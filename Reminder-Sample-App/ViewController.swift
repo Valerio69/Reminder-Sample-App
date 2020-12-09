67befbc60aeb8a04d8e20eafb554f809f54f51e7
Valerio <>
2020-12-09 15:08:01 +0100

    Initial commit

---
+++ Reminder-Sample-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder-Sample-App
+//
+//  Created by Valerio Sebastianelli on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

5117816781a85329ad484c0da3e0391575928dd6
Valerio <>
2020-12-14 08:18:17 +0100

    Added Constants

---
+++ Reminder-Sample-App/Presentation/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constnats.swift
+//  Reminder-Sample-App
+//
+//  Created by Valerio Sebastianelli on 12/14/20.
+//
+
+import Foundation
+
+
+enum Constants {
+    static let ReminderNotificationReceived = "ReminderNotificationReceived"
+    static let ReminderNotificationTapped = "ReminderNotificationReceived"
+}
